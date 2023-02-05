fbdc33425e86fb1c3ef6f58e164c1b62aef3665e
Kenny Witzenman <82523141+lovetocode14@users.noreply.github.com>
2023-02-05 06:44:31 -0600

    Create numericalQuadrature.java

---
+++ numericalQuadrature.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class numericalQuadrature
+{
+
+    public static void main(String[] args)
+    {
+        NewtonCotes.Newton();
+        GaussQuadrature.Gauss();
+        LobottoQuadrature.Lobotto();
+    }
+}
